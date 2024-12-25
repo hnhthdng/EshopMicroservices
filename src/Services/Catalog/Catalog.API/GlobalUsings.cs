e070c196ffca5edcb35913fe87171f8c7b3fef2b
Thai Duong Huynh <huynhthaiduong21042003@gmail.com>
2024-12-25 21:17:53 +0700

    implement: product features, custom exception, pipeline behaviors, seeddata and healthcheck

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -4,5 +4,15 @@ global using Carter;
 global using BuildingBlocks.CQRS;
 global using Catalog.API.Models;
 global using Marten;
+global using Marten.Pagination;
+global using Catalog.API.Exceptions;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
+global using Catalog.API.Data;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+
 
 
