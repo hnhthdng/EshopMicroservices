b1ce84a65b81563a8beba9be1fa602ea8e10648c
Thai Duong Huynh <huynhthaiduong21042003@gmail.com>
2024-12-26 11:13:46 +0700

    implement BasketRepository

--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -3,5 +3,8 @@ global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions;
 global using Marten;
 global using Basket.API.Data;
+global using Microsoft.Extensions.Caching.Distributed;
+global using System.Text.Json;
+
 
 
