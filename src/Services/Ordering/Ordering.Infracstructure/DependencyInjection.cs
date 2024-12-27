a61409a759c077858823665585efb9ee9c1e635b
Thai Duong Huynh <huynhthaiduong21042003@gmail.com>
2024-12-27 21:53:04 +0700

    clean architecture for Ordering

---
+++ src/Services/Ordering/Ordering.Infracstructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            
+            return app;
+        }
+    }
+}
