dc2b5c8c0125fac6c98738422f67065bdd2b6238
Thai Duong Huynh <huynhthaiduong21042003@gmail.com>
2024-12-28 13:43:53 +0700

    model and value object

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domain.ValueObjects;
+public record OrderName
+{
+    public string Value { get; }
+    private OrderName(string value) => Value = value;
+    public static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+
+        return new OrderName(value);
+    }
+}
