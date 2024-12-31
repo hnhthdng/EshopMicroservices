030cd80306bbc1c6644283e82564b8edd16b9eb5
Thai Duong Huynh <huynhthaiduong21042003@gmail.com>
2024-12-31 13:16:39 +0700

    host FE web into docker

---
+++ src/WebApps/Shopping.Web/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Shopping.Web.Models.Basket;
+global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Services;
+
